60ac6713bc180b99ea36669da98a9328fb92ceea
João Jacques <joaopedro_jh@hotmail.com>
2022-06-11 15:12:12 -0300

    fixed: importing and from zelus src

---
+++ scripts/test/hello_world.py
@@ -0,0 +1,24 @@
+
+# ---------------- Fixing firectory importing ----------------
+import os
+import sys
+
+current_dir = os.path.dirname(os.path.realpath(__file__))
+parent_dir  = os.path.dirname(currentdir)
+parent_parent_dir = os.path.dirname(parentdir)
+
+sys.path.append(parent_parent_dir)
+# ------------------------------------------------------------
+
+from zelus.src.repositories.access.repository import AccessRepository
+
+
+def main() -> None:
+    repo = AccessRepository()
+    text = "Alo alo teste teste"
+
+    response = repo.tweet(tweet_text = text)
+
+
+if __name__ =="__main__":
+    main()
