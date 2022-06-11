c40a0d8569b91e4f1894ee80d66beb6a7f354037
João Jacques <joaopedro_jh@hotmail.com>
2022-06-11 15:14:23 -0300

    fixed: dir variables names

--- scripts/test/hello_world.py
+++ scripts/test/hello_world.py
@@ -4,8 +4,8 @@ import os
 import sys
 
 current_dir = os.path.dirname(os.path.realpath(__file__))
-parent_dir  = os.path.dirname(currentdir)
-parent_parent_dir = os.path.dirname(parentdir)
+parent_dir  = os.path.dirname(current_dir)
+parent_parent_dir = os.path.dirname(parent_dir)
 
 sys.path.append(parent_parent_dir)
 # ------------------------------------------------------------
